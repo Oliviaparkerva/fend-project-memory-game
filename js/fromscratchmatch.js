0f0b72b983712a218a1f87a337a6df293cf37ac9
Olivia Parker <oliviaparkerva@gmail.com>
2018-05-31 11:40:19 -0400

    formatted to styling guide updated class and ID names

--- js/fromscratchmatch.js
+++ js/fromscratchmatch.js
@@ -41,7 +41,7 @@ const allCards=document.querySelectorAll('.card');//gather all the cards into a 
 let openCards=[];//create a list/array for when we open cards, so that we can compare them
 let matchedCards=[];//creates a list of cards that have been matched needs to be used to end game when length is 16
 let clicksCounted=[];//array to hold every card that has the click event triggered does not clear like openCards
-const modal = document.getElementById('successModal');
+const modal = document.getElementById('success-modal');
 const span = document.getElementsByClassName('close')[0];
 const starRating = document.getElementsByClassName('stars')[0];
 let moves=document.querySelector('span.moves');
@@ -49,6 +49,7 @@ const refreshIcon = document.querySelector('.restart');
 let timer=document.getElementById('output');
 let replayButton= document.getElementById('replay');
 let running=0;
+let successTimer=document.getElementById('final-output');
 
 /*https://stackoverflow.com/questions/7070054/javascript-shuffle-html-list-element-order*/
 function shuffleDeck(){
@@ -89,26 +90,26 @@ allCards.forEach(function(card){
             tenths=-1;
             console.log('You won');
             modal.style.display = 'block';
-            document.getElementById('attemptsTaken').innerHTML = Math.ceil(clicksCounted.length/2);//rounds down the result because there is no half a turn
-            document.getElementById('timeTaken').innerHTML = timer.innerHTML;
+            document.getElementById('attempts-made').innerHTML = Math.ceil(clicksCounted.length/2);//rounds down the result because there is no half a turn
+            document.getElementById('final-output').innerHTML = successTimer.innerHTML;
         }
 
         function starRating(){
-            if(clicksCounted.length >20){
-            document.getElementById('thirdStar').className='fa fa-star-o';
-            document.getElementById('starThree').className='fa fa-star-o';
+            if(clicksCounted.length >25){
+            document.getElementById('third-star').className='fa fa-star-o';
+            document.getElementById('star-three').className='fa fa-star-o';
 
             }
 
-            if(clicksCounted.length >30 ){
-            document.getElementById('secondStar').className='fa fa-star-o';
-            document.getElementById('starTwo').className='fa fa-star-o';
+            if(clicksCounted.length >35){
+            document.getElementById('second-star').className='fa fa-star-o';
+            document.getElementById('star-two').className='fa fa-star-o';
 
             }
 
-            if(clicksCounted.length >40 ){
-            document.getElementById('firstStar').className='fa fa-star-o';
-            document.getElementById('starOne').className='fa fa-star-o';
+            if(clicksCounted.length >45){
+            document.getElementById('first-star').className='fa fa-star-o';
+            document.getElementById('star-one').className='fa fa-star-o';
             }
 
         }
@@ -116,7 +117,7 @@ allCards.forEach(function(card){
     })
 })
 
-document.getElementById('attemptsTaken').innerHTML = clicksCounted.length;
+document.getElementById('attempts-made').innerHTML = clicksCounted.length;
 
 /*reset the html for the timer, reset the start-stop toggle to stopped*/
 refreshIcon.addEventListener('click', function (){
@@ -140,6 +141,7 @@ function addTime(){
                 }
             }
             timer.innerHTML = (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds ? (seconds > 9 ? seconds : '0' + seconds) : '00') + ':' + (tenths > 9 ? tenths : '0' + tenths);
+            successTimer.innerHTML=minutes + ' mins ' + seconds +' secs ';
             addTime();
         },100)
     }
@@ -155,6 +157,7 @@ function startStop(){
 
 function clearClock(){
     timer.innerHTML='00:00:00';
+    successTimer.innerHTML="";
     tenths=-1;/*the clock runs for a second during refresh resulting in a tenth of a second being put on the clock*/
     seconds=0;
     minutes=0;
@@ -165,7 +168,7 @@ function refreshBoard(){
     clearClock();
     shuffleDeck();
     matchedCards=[];
-    starRating.innerHTML= "<li><i id='firstStar' class='fa fa-star'></i></li><li><i id='secondStar' class='fa fa-star'></i></li><li><i id='thirdStar' class='fa fa-star'></i></li>";
+    starRating.innerHTML= "<li><i id='first-star' class='fa fa-star'></i></li><li><i id='second-star' class='fa fa-star'></i></li><li><i id='third-star' class='fa fa-star'></i></li>";
     clicksCounted=[];
     moves.innerHTML='0';
     allCards.forEach(function(card){
